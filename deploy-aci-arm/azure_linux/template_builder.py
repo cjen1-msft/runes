aecb2d65991341e0bfa5a1fb0e5b84396c8e5c1b
cjen1-msft <chrisjensen@microsoft.com>
2025-10-02 16:54:05 +0100

    Use a python side template rather than json

---
+++ deploy-aci-arm/azure_linux/template_builder.py
@@ -0,0 +1,102 @@
+import json
+
+
+class CACI:
+    def __init__(self, name, image, cpu, ram):
+        self.name = name
+        self.image = image
+        self.cpu = cpu
+        self.ram = ram
+
+    def to_dict(self, ssh_key=None):
+        if ssh_key is None:
+            cmd = ["tail", "-f", "/dev/null"]
+            env = []
+        else:
+            cmd = [
+                "/bin/sh",
+                "-c",
+                "echo Fabric_NodeIPOrFQDN=$Fabric_NodeIPOrFQDN >> /aci_env && echo UVM_SECURITY_CONTEXT_DIR=$UVM_SECURITY_CONTEXT_DIR >> /aci_env && mkdir -p /root/.ssh/ && gpg --import /etc/pki/rpm-gpg/MICROSOFT-RPM-GPG-KEY && tdnf update -y && tdnf install -y openssh-server ca-certificates && echo $SSH_ADMIN_KEY >> /root/.ssh/authorized_keys && ssh-keygen -A && sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config && sed -i 's/# PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config && /usr/sbin/sshd -D",
+            ]
+            env = [{"name": "SSH_ADMIN_KEY", "value": ssh_key}]
+
+        return {
+            "name": self.name,
+            "properties": {
+                "image": self.image,
+                "command": cmd,
+                "ports": [{"protocol": "TCP", "port": "22"}] if ssh_key else [],
+                "environmentVariables": env,
+                "volumeMounts": [],
+                "resources": {
+                    "requests": {
+                        "cpu": self.cpu,
+                        "memoryInGB": self.ram,
+                    }
+                },
+            },
+        }
+
+
+class CACIDeployment:
+    def __init__(self, name, region, sshkey=None, containers=None, acr_creds=None, ports=None):
+        self.name = name
+        self.region = region
+        self.sshkey = sshkey
+        self.containers = containers
+        self.acr_creds = acr_creds
+        if ports is not None:
+            self.ports = ports
+        else:
+            self.ports = []
+
+    def to_dict(self):
+        if self.acr_creds:
+            image_crds = {
+                "imageRegistryCredentials": [
+                    {
+                        "server": self.acr_creds["server"],
+                        "username": self.acr_creds["username"],
+                        "password": self.acr_creds["password"],
+                    }
+                ]
+            }
+        else:
+            image_crds = {}
+
+        ports = self.ports
+        if self.sshkey:
+          ports += [{"protocol": "TCP", "port": "22"}]
+
+        return {
+            "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+            "contentVersion": "1.0.0.0",
+            "parameters": {},
+            "variables": {},
+            "resources": [
+                {
+                    "type": "Microsoft.ContainerInstance/containerGroups",
+                    "apiVersion": "2022-10-01-preview",
+                    "name": self.name,
+                    "location": self.region,
+                    "identity": {"type": "SystemAssigned"},
+                    "properties": {
+                        "sku": "Confidential",
+                        "restartPolicy": "Never",
+                        "osType": "Linux",
+                        "ipAddress": {
+                            "ports": self.ports,
+                            "type": "Public",
+                        },
+                        "volumes": [],
+                        "confidentialComputeProperties": {
+                            "ccePolicy": "cGFja2FnZSBwb2xpY3kKCmFwaV9zdm4gOj0gIjAuMTAuMCIKZnJhbWV3b3JrX3N2biA6PSAiMC4xLjAiCgptb3VudF9kZXZpY2UgOj0geyJhbGxvd2VkIjogdHJ1ZX0KbW91bnRfb3ZlcmxheSA6PSB7ImFsbG93ZWQiOiB0cnVlfQpjcmVhdGVfY29udGFpbmVyIDo9IHsiYWxsb3dlZCI6IHRydWUsICJhbGxvd19zdGRpb19hY2Nlc3MiOiB0cnVlfQp1bm1vdW50X2RldmljZSA6PSB7ImFsbG93ZWQiOiB0cnVlfQp1bm1vdW50X292ZXJsYXkgOj0geyJhbGxvd2VkIjogdHJ1ZX0KZXhlY19pbl9jb250YWluZXIgOj0geyJhbGxvd2VkIjogdHJ1ZX0KZXhlY19leHRlcm5hbCA6PSB7ImFsbG93ZWQiOiB0cnVlLCAiYWxsb3dfc3RkaW9fYWNjZXNzIjogdHJ1ZX0Kc2h1dGRvd25fY29udGFpbmVyIDo9IHsiYWxsb3dlZCI6IHRydWV9CnNpZ25hbF9jb250YWluZXJfcHJvY2VzcyA6PSB7ImFsbG93ZWQiOiB0cnVlfQpwbGFuOV9tb3VudCA6PSB7ImFsbG93ZWQiOiB0cnVlfQpwbGFuOV91bm1vdW50IDo9IHsiYWxsb3dlZCI6IHRydWV9CmdldF9wcm9wZXJ0aWVzIDo9IHsiYWxsb3dlZCI6IHRydWV9CmR1bXBfc3RhY2tzIDo9IHsiYWxsb3dlZCI6IHRydWV9CnJ1bnRpbWVfbG9nZ2luZyA6PSB7ImFsbG93ZWQiOiB0cnVlfQpsb2FkX2ZyYWdtZW50IDo9IHsiYWxsb3dlZCI6IHRydWV9CnNjcmF0Y2hfbW91bnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0Kc2NyYXRjaF91bm1vdW50IDo9IHsiYWxsb3dlZCI6IHRydWV9Cg=="
+                        },
+                        "containers": [container.to_dict(self.sshkey) for container in self.containers],
+                    } | image_crds,
+                }
+            ],
+        }
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=4)
